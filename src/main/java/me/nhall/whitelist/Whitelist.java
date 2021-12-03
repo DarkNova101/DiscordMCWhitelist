4f42951bcc15e3596f33c78c3889740b39abd11f
Nolan <nehall.neh@gmail.com>
2021-12-02 21:26:11 -0600

    Discord Whitelist Command
    Added /whitelist to the discord bot for anybody with the GO's role in the discord server. This will allow people with said role to whitelist players to the server when I am unavailable.

--- src/main/java/me/nhall/whitelist/Whitelist.java
+++ src/main/java/me/nhall/whitelist/Whitelist.java
@@ -2,28 +2,42 @@ package me.nhall.whitelist;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
 
 public final class Whitelist extends JavaPlugin {
 
     private JDA jda;
+    private FileConfiguration config;
+    private static Whitelist plugin;
 
     @Override
     public void onEnable() {
+        if (!this.getDataFolder().exists()) {
+            this.saveDefaultConfig();
+        }
+        config = this.getConfig();
         try {
-            jda = JDABuilder.createDefault("").build();
+            jda = JDABuilder.createDefault(config.getString("token")).build();
         } catch (LoginException e) {
             e.printStackTrace();
         }
-        if(jda == null) {
+        if (jda == null) {
             this.getPluginLoader().disablePlugin(this);
         }
+
     }
 
     @Override
     public void onDisable() {
 
     }
+
+    public static Whitelist getPlugin() {
+        return plugin;
+    }
+
 }
---
+++ src/main/java/me/nhall/whitelist/command/discord/DiscordWhitelistCmd.java
@@ -0,0 +1,27 @@
+package me.nhall.whitelist.command.discord;
+
+import me.nhall.whitelist.Whitelist;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
+
+public class DiscordWhitelistCmd extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommand(@NotNull SlashCommandEvent event) {
+        if (event.getName().equals("whitelist")) {
+            event.deferReply().queue();
+            Role role = event.getGuild().getRoleById(Whitelist.getPlugin().getConfig().getInt("role"));
+            if (!event.getMember().getRoles().contains(role)) {
+                event.getHook().sendMessage("You do not have the required permissions.").queue();
+                return;
+            }
+
+            String username = event.getOption("username").getAsString();
+            Whitelist.getPlugin().getServer().dispatchCommand(Bukkit.getConsoleSender(), "whitelist add " + username);
+            event.getHook().sendMessage("Added " + username + " to the server whitelist.").queue();
+        }
+    }
+}
